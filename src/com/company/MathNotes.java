6b49f57a581a0fd8832e13da34a0a8d9f0e89c90
Kymbrlee <kymba7@gmail.com>
2018-01-10 16:14:50 -0700

    Math Notes

---
+++ src/com/company/MathNotes.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class MathNotes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        int scottsnumber = 100;
+        scottsnumber = scottnumber + 100;
+        scottsnumber += 100; // Line 10 & 9 are the smae htings
+
+        int minusNumber = minusNumber = 100;
+        minusNumber-= 100;
+
+        int multiNumber = multiNumber * 100;
+        multiNumber *= 100;
+
+        int divNumber = divNumber / 100;
+        divNumber /= 100;
+
+
+        int iteration = 1;
+        iteration ++; //takes the iteration variable and adds 1 to it
+        iteration --; // takes the iteration variable and subtracts 1 from it
+
+        int mod = 10;
+        mod = mod % 3;
+        System.out.println("Mod: " + mod);
+
+
+        public class MathNotes {
+
+            public static void main(String[] args) {
+                System.out.println("Jello!");
+
+                int addNumber = 100;
+                addNumber = addNumber + 100;
+                addNumber += 100; // Line 10 and Line 9 do the same thing
+
+                int minusNumber = 500;
+                minusNumber = minusNumber - 100;
+                minusNumber -= 100; // Line 14 and Line 13 do the same thing
+
+                int multiNumber = 100;
+                multiNumber = multiNumber * 100;
+                multiNumber *= 100; // Line 18 and Line 17 do the same thing
+
+                int divNumber = 10000;
+                divNumber = divNumber / 100;
+                divNumber /= 100; // Line 22 and Line 21 do the same thing
+
+                System.out.println("Add: " + addNumber);
+                System.out.println("Sub: " + minusNumber);
+                System.out.println("Multiply: " + multiNumber);
+                System.out.println("Division: " + divNumber);
+
+                int iteration = 1;
+                iteration++; //takes the iteration variable and adds 1 to it.
+                iteration--; //takes the iteration variable and subtracts 1 from it.
+
+                int mod = 10;
+                mod = mod % 3;
+                System.out.println("Mod: " + mod);
+    }
+}
